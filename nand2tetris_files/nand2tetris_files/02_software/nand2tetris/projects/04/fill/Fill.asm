a624d83f64a7e8cdf509ec27f063433aa665fd22
Gourlie <gourlik@linnbenton.edu>
2024-05-09 19:14:13 -0700

    assign_4

--- nand2tetris_files/nand2tetris_files/02_software/nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris_files/nand2tetris_files/02_software/nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,73 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//Note that I did not work on this code independently. I had alot of struggle starting this portion of the 
+//project, and heavily relied on online resources for its completion. I followed along how another person
+//completed the assignment and understood the process. This is why I included many comments through the 
+//code to not only better my understanding, but to show that I actually understand what is going on. 
+
+//R0: 16-bit integer. If -1, then program enters ON. If 0, then program enters OFF
+//R1: Row counter. 
+//R2: current position holder
+//LOOP: Main loop where keyboard input is always being sensed
+//ON: Loop for if Keyboard is pressed; for blackshading
+//OFF: Loop for it Keyboard is NOT pressed; for whiteshading
+//ACTION: Loop for keeping track of pixel location and filling/unfilling pixels
+//INRCEM: Loop for incrementing each row of the SCREEN, which there are 255 of them
+
+(LOOP)
+    @KBD    //Keyboard register value, 16384 
+    D=M     //sets D as Keyboard register value
+    //If KBD>0, executes ON
+    @ON
+    D;JGT   
+    //If KBD=0, executes OFF
+    @OFF
+    D;JEQ 
+
+(ON)
+    @R0
+    //Setting M=-1 allows for setting 16 pixels black at a time
+    M=-1    //-1 = 1111111111111111
+    @ACTION 
+    0;JMP
+
+(OFF)
+    @R0
+    M=0
+    @ACTION
+    0;JMP
+
+(ACTION)
+//since one row is 512 bit and I can assign 16 bits at a time, i want an increment of 512*16=8192
+    @8191   
+    D=A 
+    @R1 //setting increment counter to 8191
+    M=D
+    (INCREM)
+        @R1     
+        D=M     //setting D to be total number of iterations I need to do per row
+        @R2
+        M=D     //setting R2 to be total number of iterations I need to do per row
+        @SCREEN 
+        D=A    //getting D equaling to the register address of the SCREEN
+        @R2
+        M=M+D  //incremented by one row
+
+        @R0
+        D=M    //setting D= -1 or 0, which maps back to SCREEN
+        @R2 
+        A=M //memory mapping R0 to R2, in which points to SCREEN meory address
+        M=D //changing the address of R2 to map to ON (-1) or OFF (0)
+
+        //removing 1 row because 1 row has been filled or unfilled
+        @R1
+        D=M-1
+        //resetting counter
+        M=D
+        //if still needs to go through rest of rows so loop until D=0
+        @INCREM
+        D;JGE
+    @LOOP
+    0;JMP
+    
--- nand2tetris_files/nand2tetris_files/02_software/nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris_files/nand2tetris_files/02_software/nand2tetris/projects/04/mult/Mult.asm
@@ -9,4 +9,30 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @i      //i is the counter
+    M=0     //setting counter initially to 0
+    @R2     //R2 is where the product will be stored
+    M=0     //setting product to be initially 0
+(LOOP)
+    @i      //accessing i
+    D=M     //setting D equal to memory of i
+    @R1     //accessing R1 
+    D=D-M   //checking to see if i==R1 
+    @END 
+    D;JEQ   //if i==R1, then JUMP to END
+
+    @R0     //accessing R0
+    D=M     //setting D equal to memory of R0
+    @R2     //accessing R2
+    M=M+D   //repeated addition to represent multiplication
+
+    @i      //accessing i
+    M=M+1   //incrementing i by 1
+    @LOOP   //starting the LOOP
+    0;JMP   //JUMP to LOOP
+
+(END)       //infinite loop
+    @END
+    0;JMP
+
+    
